c7e3b132823fb36c6bcbc7845c8e752a06776467
Huỳnh Quốc Đạt <dat@vuta.vn>
2021-10-28 23:17:45 +0700

    add code smartcontract

---
+++ contract/~nftContract.sol
@@ -0,0 +1,26 @@
+// contracts/GameItem.sol
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
+import "@openzeppelin/contracts/utils/Strings.sol";
+
+contract animalNftMarket is ERC721{
+    using Strings for uint256;
+    
+    string public domain = "http://103.153.214.201:3000/animal/detail/";
+    
+    constructor(string memory name, string memory sympol) ERC721(name, sympol){}
+    
+    function tokenURI(uint256 tokenId) public view virtual override returns(string memory){
+        require(_exists(tokenId), "TokenId is not availble.");
+        return string(abi.encodePacked(domain, tokenId.toString()));
+    } 
+    
+    function create_animal(uint256 tokenId) external payable{
+        require(msg.value>0, "Animal are not free!!");
+        _mint(msg.sender, tokenId);
+    }
+}
